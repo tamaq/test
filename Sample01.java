e44ff2c85b20b6c1636b66b6ade24622d11ba6c0
tamaq <queiwa99@gmail.com>
2019-01-07 16:49:11 +0900

    文書を２行に増やしました

--- Sample01.java
+++ Sample01.java
@@ -1,5 +1,7 @@
 public class Sample01{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("HelloGit&GitHub");
+		System.out.println("testtest");
+
 	}
 }	
